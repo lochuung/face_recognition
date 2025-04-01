f8f3d33d154b8ffc55f5c0068b6d4be95d5ba8ce
Nguyen Huu Loc <locn562836@gmail.com>
2025-04-01 14:21:11 +0700

    feat: Add requirements file and update face sample collection for new user

--- predict.py
+++ predict.py
@@ -8,7 +8,7 @@ YUNET_MODEL = "model/face_detection_yunet_2023mar.onnx"
 SFACENET_MODEL = "model/face_recognition_sface_2021dec.onnx"
 
 svc = joblib.load('model/svc.pkl')
-mydict = ['Chi Thanh', 'Huu Loc', 'Linh Phan', 'Thai Hung']
+mydict = ['Bao', 'Chi Thanh', 'Huu Loc', 'Linh Phan', 'Thai Hung']
 
 
 def str2bool(v):
